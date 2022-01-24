9882927afcc775d905a882bc61cb4e274d09f6f3
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-01-24 08:03:10 -0500

    Update program.cs to include connection string

--- DEBUG_COPPO_API/Program.cs
+++ DEBUG_COPPO_API/Program.cs
@@ -1,12 +1,22 @@
+using DEBUG_COPPO_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// Add swagger openApi.
 builder.Services.AddSwaggerGen();
 
+// Add connection to the database.
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Build the application this step is done once the services for the application have been set.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,10 +26,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Add middleware for redirecting HTTP Requests to HTTPS.
 app.UseHttpsRedirection();
 
+// Add middleware that will enable authentication.
 app.UseAuthorization();
 
+// Add end points for controller actions.
 app.MapControllers();
 
+// Runs an application and block the calling thread until host shutdown.
 app.Run();
