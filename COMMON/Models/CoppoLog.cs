a2af60b8eade4fe5711a4b651ed90fb17503a08e
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-04-01 21:25:19 -0400

    Update CoppoLog.cs

--- COMMON/Models/CoppoLog.cs
+++ COMMON/Models/CoppoLog.cs
@@ -2,45 +2,45 @@
 namespace COMMON.Models;
 
 /// <summary>
-/// A <see cref="Log"/> class which contains application logging information for the COPPO_API project.
+/// A <see cref="CoppoLog"/> class which contains application logging information for the COPPO_API project.
 /// </summary>
-public class Log
+public class CoppoLog
 {
     /// <summary>
-    /// A unique identifier used to identity this <see cref="Log"/>.
+    /// A unique identifier used to identity this <see cref="CoppoLog"/>.
     /// </summary>
     [Key]
     [Required]
     public int Id { get; }
 
     /// <summary>
-    /// The information about this <see cref="Log"/>.
+    /// The information about this <see cref="CoppoLog"/>.
     /// </summary>
     public string? Message { get; set; }
     
     /// <summary>
-    /// The structured representation of the message for this <see cref="Log"/>
+    /// The structured representation of the message for this <see cref="CoppoLog"/>
     /// </summary>
     public string? MessageTemplate { get; set; }
 
     /// <summary>
-    /// This is the serilog level for the <see cref="Log"/> event, they are six levels of serilog which are
+    /// This is the serilog level for the <see cref="CoppoLog"/> event, they are six levels of serilog which are
     /// Verbose, debug, information, warning, Error and fatal. 
     /// </summary>
     public string? Level { get; set; } 
     
     /// <summary>
-    /// The time in which the <see cref="Log"/> even occured.
+    /// The time in which the <see cref="CoppoLog"/> even occured.
     /// </summary>
     public DateTime TimeStamp { get; set; }
     
     /// <summary>
-    /// The <see cref="Exception"/> if any contained by the current <see cref="Log"/>.
+    /// The <see cref="Exception"/> if any contained by the current <see cref="CoppoLog"/>.
     /// </summary>
     public string? Exception { get; set; } 
 
     /// <summary>
-    /// Describes a set of attributes that belong to a <see cref="Log"/>.
+    /// Describes a set of attributes that belong to a <see cref="CoppoLog"/>.
     /// </summary>
     public string? Properties { get; set; }
 }
