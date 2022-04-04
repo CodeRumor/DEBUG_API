cd2e6a974446730a1605c148e6baa9aebe8344c8
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-04-03 22:36:41 -0400

    Update the location of the Application Db context file

---
+++ DEBUG_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,43 @@
+using CodeRumor.DataAccessLibrary.DbContext;
+using COMMON.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEBUG_API.Data;
+
+/// <summary>
+/// An implementation of the <see cref="ApplicationDbContext"/> class used to access data from the database.
+/// </summary>
+public class ApplicationDbContext : GeneralDbContext, IApplicationDbContext
+{
+    /// <summary>
+    /// Initialises an instance of the <see cref="ApplicationDbContext"/> class.
+    /// </summary>
+    /// <param name="options">
+    /// Carries the configuration information needed to configure the <see cref="DbContext"/>.
+    /// </param>
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    /// <summary>
+    /// This method is called when the model for a derived context has been initialized and is used to create
+    /// relationships between models.
+    /// </summary>
+    /// <param name="modelBuilder">
+    /// Provides an API which is used to configure the shape, data type and relationship between models.
+    /// </param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // create a Logs table with auto id.
+        modelBuilder.Entity<CoppoLog>().ToTable("CoppoLogs");
+        modelBuilder.Entity<CoppoLog>().Property(log => log.Id).ValueGeneratedOnAdd();
+        
+        modelBuilder.Entity<CoppoLog>(entity =>
+        {
+            entity.Property(e => e.TimeStamp).HasColumnType("datetime");
+        });
+    }
+    
+    /// <inheritdoc/>
+    public DbSet<CoppoLog>? CoppoLogs { get; set; }
+}
