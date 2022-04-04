ac55b7f6e79b628f60f98100a999e5036813a061
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-04-03 22:37:00 -0400

    Update the location of the IApplication db context file

---
+++ DEBUG_API/Data/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using COMMON.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEBUG_API.Data;
+
+/// <summary>
+/// A blue print of the <see cref="ApplicationDbContext"/> class that provides a context used to query data from the
+/// database.
+/// </summary>
+public interface IApplicationDbContext
+{
+    /// <summary>
+    /// Gets or sets <see cref="CoppoLog"/> data in the database.
+    /// </summary>
+    public DbSet<CoppoLog>? CoppoLogs { get; set; }
+}
